630a0fc8447b0f9a4fb69544870013d331e1282d
Erik Anderson <itserikanderson@gmail.com>
2015-07-15 10:45:47 -0400

    scoring updated, end game updated.

--- script.js
+++ script.js
@@ -5,17 +5,21 @@ var timer;
 var question= 0;
 var questionNumber= 0;
 var answer;
-
 //arrays of questions, answers, and solutions
 var defaultIndex = ["triv.i.al", "A","B","C","D"]
-var questionIndex = ["What is the best selling album of all time?", "Who invented the electric guitar?", "Who won the first Rap Performance Grammy in 1989?", "Who has the longest running number one Billboard hit?", "Who holds the record for longest concert?"];
+var questionIndex = ["What is the best selling album of all time?", "Who invented the electric guitar?", "Who won the first Rap Performance Grammy in 1989?", "Who has the longest running number one Billboard hit?", "Who holds the record for longest concert?", "What artist has the most number one Singles?", "Which artist has never had a number one song?", "Which song does not contain any samples?", "What was the first hip hop song to chart on Billboard?","What band has had the most members from start to finish?", "Good Game!"];
 var answerIndex = [["Michael Jackson's, Thriller", "The Beatles, White Album", "Britney  Spears, Baby One More Time", "Back Street Boys, Millenium"],
 ["Leo Fender", "Orville Gibson", "Adolph Rickenbacker", "Les Paul"],
 ["Nas", "DJ Jazzy Jeff & the Fresh Prince", "LL Cool J", "Eric B and Rakim"],
 ["Mariah Carey & Boyz II Men", "Elvis", "Whitney Houston", "Carlos Santana"],
-["Grateful Dead", "Phish", "Broken Social Scene", "Gonzales"]];
-var correctIndex = ["Michael Jackson's, Thriller", "Adolph Rickenbacker", "DJ Jazzy Jeff & the Fresh Prince", "Mariah Carey & Boyz II Men", "Gonzales"]
-var wrongIndex = ["#b,#c,#d", "#a, #b, #d", "#a, #c, #d", "#d, #b, #c", "#a, #b, #c"]
+["Grateful Dead", "Phish", "Broken Social Scene", "Gonzales"],
+["Madonna", "Janet Jackson", "The Supremes", "The Beatles"],
+["Bruce Springsteen", "Beyonce", "Stevie Wonder", "Katy Perry"],
+["Michael Jackson - Beat It", "Daft Punk - One More Time", "Kanye West- All of the Lights", "The Beatles - Come Together"],
+["Planet Rock - Afrika Bambaataa", "Sugarhill Gang - Rapper's Delight", "Beastie Boys - Brass Monkey"],
+["Santana", "Guns N Roses", "AC/DC", "Oasis" ],
+["","","",""]];
+var correctIndex = ["Michael Jackson's, Thriller", "Adolph Rickenbacker", "DJ Jazzy Jeff & the Fresh Prince", "Mariah Carey & Boyz II Men", "Gonzales", "The Beatles", "Bruce Springsteen", "Kanye West- All of the Lights", "Sugarhill Gang - Rapper's Delight", "Santana"];
 
 $( document ).ready(function() {
 /////////////////////////////////////
@@ -31,7 +35,6 @@ $("#start").on("click", function(){
   questionNumber++;
   $("#questionNumber").text(questionNumber)
   console.log("button click");
-  // checkAnswer();
 
 //start timer
   timer = setInterval(function(){
@@ -46,13 +49,18 @@ $("#start").on("click", function(){
 $(".answer").on("click", function(){
   console.log($(this).text());
   answer = $(this).text();
+
   if (answer === correctIndex[question]){
   console.log("correct")
   alert("correct!")
   score = score + 1000;
   $("#score").text(score);
 }
-else {
+  else if (answer === ""){
+    alert("good game!")
+    reset();
+  }
+  else {
   console.log("Wrong")
   alert("Wrong!")
   score = score - 1000;
@@ -60,17 +68,18 @@ else {
 }
   clearInterval(timer);
   $("#next").css("display", "block")
-
   }
 )
 
 /////////////////////////////////////
 // next question function
 $("#next").on("click", function(){
+  var goodGame = $("#question").text;
   clearInterval(timer)
   seconds = 0
   question ++;
   questionNumber ++;
+  else {
   timer = setInterval(function(){
   seconds++;
   $("#questionNumber").text(questionNumber)
@@ -84,12 +93,12 @@ $("#next").on("click", function(){
     $("#b").text(answerIndex[question][1])
     $("#c").text(answerIndex[question][2])
     $("#d").text(answerIndex[question][3])
-    // checkAnswer();
+    }
   }
 )
 ///////////////////////////////////////
 //reset button
-$("#reset").on("click", function(){
+$("#reset").on("click", function reset (){
 $("#start").css("display", "inline")
 clearInterval(timer)
 seconds = 0;
@@ -104,44 +113,9 @@ $("#questionNumber").text("")
     $("#b").text(defaultIndex[2])
     $("#c").text(defaultIndex[3])
     $("#d").text(defaultIndex[4])
+  $("#next").css("display", "none")
   }
 )
-//Scoring
-// function checkAnswer(){
-//   $(correctIndex[question]).on("click", function right (){
-//     console.log("score!")
-//     console.log(correctIndex[question]);
-//       alert("correct!")
-//       score = score + 1000;
-//     $("#score").text(score)
-//     $(correctIndex[question]).off("click", right);
-//     })
-//   $(wrongIndex[question]).on("click", function wrong (){
-//     console.log("wronganswer");
-//     console.log("wrongIndex[question]")
-//     alert("Wrong!")
-//     score = score - 1000;
-//     $("#score").text(score)
-//     $(wrongIndex[question]).off("click", wrong);
-//   })
-// }
-// function checkAnswer(){
-//   if(questionNumber === 1){
-//     console.log("works1")
-//   }
-//   else if (questionNumber === 2){
-//     console.log("works2");
-//   }
-//   else if (questionNumber === 3){
-//     console.log("works3");
-//   }
-//   else if (questionNumber === 4){
-//     console.log("works4");
-//     }
-//   else if(questionNumber === 5){
-//     console.log("works5");
-//     }
-// }
 /////fin
   }
 )
